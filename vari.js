3f018191c82433c2563ce1fd00bddd094b0eaad2
VKSawna <160307850+VKSawna@users.noreply.github.com>
2025-06-16 19:12:51 +0530

    Add files via upload

---
+++ vari.js
@@ -0,0 +1,17 @@
+let num1 = 10; // This variable is created using 'let' and is changeable
+var num2 = 50; // This variable is created using 'var' and is also changeable
+const num3 = 100; // This variable is created using 'const' and is not changeable
+username = "Varinder";
+num = null;
+let hello;
+console.log("num1 "+ num1);
+console.log("num2 "+ num2);
+console.log("num3 "+ num3);
+console.log(typeof hello);
+console.log( num);
+// The above code demonstrates the use of different variable declarations in JavaScript
+num1 += 20;
+num2 += 10;
+console.log("Updated num1 "+ num1);
+console.log("Updated num2 "+ num2);
+// num3 += 50; // this line would cause an error because num3 is a constant and cannot be changed
